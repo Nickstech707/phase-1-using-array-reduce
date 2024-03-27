238ee09d29f54a00bd9ed4fd7ac9ffcd16955cce
Nicholas Muriithi <nickstech707@gmail.com>
2024-03-28 00:22:44 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// Calculates the total number of batteries using the reduce method
+const totalBatteries = batteryBatches.reduce(function(total, batches) {
+    // Adds the current batch count to the total
+    return batches + total;
+  });
+
